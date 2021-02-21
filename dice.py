4bce0b1167c889ef73a4a77021e52b7f14fd1fd6
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-02-21 14:16:27 -0700

    Added random image to program

--- dice.py
+++ dice.py
@@ -2,6 +2,8 @@ import tkinter
 from PIL import Image, ImageTk 
 import random
 
+
+
 # Widgit that represents the main window for the application
 root = tkinter.Tk("Test screen")
 root.title('Dice Rolling')
@@ -12,9 +14,19 @@ blankline = tkinter.Label(root, text='')
 blankline.pack()
 
 # Adding a header in the box
-Heading = tkinter.Label(root, text='Roll the Dice!', fg = 'black', bg = 'light blue', font = 'Helvetica 20 bold')
-Heading.pack()
+heading = tkinter.Label(root, text='Roll the Dice!', fg = 'black', bg = 'light blue', font = 'Helvetica 20 bold')
+heading.pack()
 
+dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
+
+# Selecting a random image to display on screen
+dice_image = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+# Constructing a label widget for the image
+image_label = tkinter.Label(root, image= dice_image)
+image_label.image = dice_image
+
+image_label.pack(expand = True)
 
 # This keeps the windo open
 root.mainloop()
