a07ea8e22ce9ea54329582854a7f1adcdca0da41
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-02-21 14:11:00 -0700

    Added dice image files and added header to screen

--- dice.py
+++ dice.py
@@ -8,5 +8,13 @@ root.title('Dice Rolling')
 # Setting the screen size in px
 root.geometry('400x400')
 
+blankline = tkinter.Label(root, text='')
+blankline.pack()
+
+# Adding a header in the box
+Heading = tkinter.Label(root, text='Roll the Dice!', fg = 'black', bg = 'light blue', font = 'Helvetica 20 bold')
+Heading.pack()
+
+
 # This keeps the windo open
 root.mainloop()
