e06439c0076f089076efc334076d865b75ddaea8
nicwenzel26 <nwenzel@mymail.mines.edu>
2021-02-21 14:04:07 -0700

    Got init blank screen working

---
+++ dice.py
@@ -0,0 +1,12 @@
+import tkinter
+from PIL import Image, ImageTk 
+import random
+
+# Widgit that represents the main window for the application
+root = tkinter.Tk("Test screen")
+root.title('Dice Rolling')
+# Setting the screen size in px
+root.geometry('400x400')
+
+# This keeps the windo open
+root.mainloop()
